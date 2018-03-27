04813285c7b55f0382b8599d18914573985e7a38
abigail1993 <alex.abi.roja@gmail.com>
2018-03-27 15:19:43 -0600

    ejercicio del 2703

---
+++ app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*
+class App extends React.Component {
+  render(){
+    return(<div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO2ewS0xpcxeIeqCgJ4DZdyJzs31Ub3M3sEQ3NX-373zKi6g_x" />
+        <p>Abigail</p>
+        <p>25 años</p>
+      </div>
+    )
+  }
+}*/
+
+ReactDOM.render(<App />, document.getElementById("container-react"))
